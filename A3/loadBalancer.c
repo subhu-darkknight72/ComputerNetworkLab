c87889f7c55d25ad6580f6b2c37b418c21aa9ea0
Subhajyoti Halder <subhu@Subhajyotis-MacBook-Air.local>
2023-01-27 00:37:27 +0530

    Working totally

--- A3/loadBalancer.c
+++ A3/loadBalancer.c
@@ -7,6 +7,9 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include <poll.h>
+#include <time.h>
+
 const int BUF_SIZE = 1001; //	Packet size for transmission
 const int MAX_SIZE = 1001; //	Max length of strings to be ahndle in operations
 void sendStr(char *str, int socket_id);
@@ -19,8 +22,9 @@ int main(int argc, char *argv[])
 	int sockfd_s[2];
 	struct sockaddr_in cli_addr, serv_addr, serv1_addr, serv2_addr;
 
-	int s1_port = atoi(argv[2]);
-	int s2_port = atoi(argv[3]);
+	int s_port[2];
+	s_port[0] = atoi(argv[2]);
+	s_port[1] = atoi(argv[3]);
 	int c0_port = atoi(argv[1]);
 
 	char buf[MAX_SIZE]; /* We will use this buffer for communication */
@@ -48,79 +52,108 @@ int main(int argc, char *argv[])
 	while (1)
 	{
 		if ((sockfd_s[0] = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		printf("Cannot create server-socket-1\n");
-		exit(0);
-	}
+		{
+			printf("Cannot create server-socket-1\n");
+			exit(0);
+		}
 
-	serv1_addr.sin_family = AF_INET;
-	inet_aton("127.0.0.1", &serv1_addr.sin_addr);
-	serv1_addr.sin_port = htons(s1_port);
+		serv1_addr.sin_family = AF_INET;
+		inet_aton("127.0.0.1", &serv1_addr.sin_addr);
+		serv1_addr.sin_port = htons(s_port[0]);
 
-	if ((connect(sockfd_s[0], (struct sockaddr *)&serv1_addr, sizeof(serv1_addr))) < 0)
-	{
-		perror("Unable to connect to server-1\n");
-		exit(0);
-	}
-	else
-		printf("Connected to server-1 successfully!!\n");
-	int l0, l1, k = 0;
+		if ((connect(sockfd_s[0], (struct sockaddr *)&serv1_addr, sizeof(serv1_addr))) < 0){
+			perror("Unable to connect to server-1\n");
+			exit(0);
+		}
+		else
+			printf("Connected to server-1 successfully!!\n");
+		int l0, l1;
 
+		strcpy(buf, "load");
+		sendStr(buf, sockfd_s[0]);
 		receiveStr(buf, sockfd_s[0]);
 		l0 = atoi(buf);
+		close(sockfd_s[0]);
 
-	if ((sockfd_s[1] = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		printf("Cannot create server-socket-2\n");
-		exit(0);
-	}
+		if ((sockfd_s[1] = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+			printf("Cannot create server-socket-2\n");
+			exit(0);
+		}
 
-	serv2_addr.sin_family = AF_INET;
-	inet_aton("127.0.0.1", &serv2_addr.sin_addr);
-	serv2_addr.sin_port = htons(s2_port);
+		serv2_addr.sin_family = AF_INET;
+		inet_aton("127.0.0.1", &serv2_addr.sin_addr);
+		serv2_addr.sin_port = htons(s_port[1]);
+
+		if ((connect(sockfd_s[1], (struct sockaddr *)&serv2_addr, sizeof(serv2_addr))) < 0){
+			perror("Unable to connect to server-2\n");
+			exit(0);
+		}
+		else
+			printf("Connected to server-2 successfully!!\n");
 		
-
-	if ((connect(sockfd_s[1], (struct sockaddr *)&serv2_addr, sizeof(serv2_addr))) < 0)
-	{
-		perror("Unable to connect to server-2\n");
-		exit(0);
-	}
-	else
-		printf("Connected to server-2 successfully!!\n");
-
+		strcpy(buf, "load");
+		sendStr(buf, sockfd_s[1]);
 		receiveStr(buf, sockfd_s[1]);
 		l1 = atoi(buf);
-
+		close(sockfd_s[1]);
 
 		printf("Server1 Load: %d\n", l0);
 		printf("Server2 Load: %d\n", l1);
 
-		clilen = sizeof(cli_addr);
-		newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr,
-						   (socklen_t *)&clilen);
+		int timeout = 5000;
+		while(timeout>0){
+			struct pollfd fdset[1];
+			fdset[0].events = POLLIN;	fdset[0].fd = sockfd;
 
-	
-		if (l1 < l0)
-			k = 1;
-			
-		strcpy(buf, "kill");
-		sendStr(buf, sockfd_s[1 - k]);
+			time_t t1 = time(NULL);
+			int pval = poll(fdset, 1, timeout);
+			time_t t2 = time(NULL);
+			timeout = difftime(t2, t1);
 
-		strcpy(buf, "time");
-		sendStr(buf, sockfd_s[k]);
-		receiveStr(buf, sockfd_s[k]);
+			if(pval <0)
+				printf("Error in Polling!!");
+			else if(pval>0){
+				clilen = sizeof(cli_addr);
+				newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr,(socklen_t *)&clilen);
+				printf("Client Connected!!\n");
 
-		sendStr(buf, newsockfd);
+				int k = 0;
+				if (l1 < l0)	k = 1;
+				printf("min loaded-server: %d\n",k);
 
-		receiveStr(buf, newsockfd);
-		printf("%s\n", buf);
+				if(fork()==0){
+					close(sockfd);
+					//	communicate connect
+					if ((sockfd_s[k] = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+						printf("Cannot create server-socket-1\n");
+						exit(0);
+					}
 
-		strcpy(buf, "Message from client to Server-1");
-		sendStr(buf, sockfd_s[0]);
+					serv1_addr.sin_family = AF_INET;
+					inet_aton("127.0.0.1", &serv1_addr.sin_addr);
+					serv1_addr.sin_port = htons(s_port[k]);
 
-		strcpy(buf, "Message from client to Server-2");
-		sendStr(buf, sockfd_s[1]);
-		close(newsockfd);
+					if ((connect(sockfd_s[k], (struct sockaddr *)&serv1_addr, sizeof(serv1_addr))) < 0){
+						perror("Unable to connect to server\n");
+						exit(0);
+					}
+					
+					strcpy(buf, "time");
+					sendStr(buf, sockfd_s[k]);
+					receiveStr(buf, sockfd_s[k]);
+					
+					printf("%s\n",buf);	
+					sendStr(buf, newsockfd);
+					
+					close(sockfd_s[k]);
+					close(newsockfd);
+					exit(0);
+				}
+				close(newsockfd);
+			}
+
+
+		}
 	}
 	close(sockfd_s[0]);
 	close(sockfd_s[1]);
