76138b696e8fc604b3052baccfdb50cf95590e95
Subhajyoti Halder <subhu@Subhajyotis-MacBook-Air.local>
2023-02-11 06:54:38 +0530

    Added sample working code

---
+++ A4/http_client.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
+
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8080/");
+
+    res = curl_easy_perform(curl);
+
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+
+    curl_easy_cleanup(curl);
+  }
+
+  curl_global_cleanup();
+
+  return 0;
+}
