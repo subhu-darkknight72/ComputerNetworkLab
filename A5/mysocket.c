4a45b715e56ae702c6addb943cbcf31151cb7b6e
Subhajyoti Halder <subhu@Subhajyotis-MacBook-Air.local>
2023-03-05 21:40:37 +0530

    createPushShell

--- A5/mysocket.c
+++ A5/mysocket.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
 int mul(int a,int b){
     return a*b;
 }
--- A5/mysocket.h
+++ A5/mysocket.h
@@ -1,1 +1,2 @@
 int mul(int a, int b);
+my_socket
